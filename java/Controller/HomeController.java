87dda62ea9d79b9a81ca129b09039c5463d081d9
wkdgus1164 <wkdgus1164@naver.com>
2019-02-08 16:30:47 +0900

    init_folder

--- java/Controller/HomeController.java
+++ java/Controller/HomeController.java
@@ -1,39 +1,74 @@
 package Controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
-	}
-	
+    public static final String VIEW_PATH = "/WEB-INF/views/";
+
+    /* index */
+    @RequestMapping("/")
+    public String index() {
+        return VIEW_PATH + "index.jsp";
+    }
+
+    /* for seller start */
+    @RequestMapping("/seller/login.do")
+    public String sellerLogin() {
+        return VIEW_PATH + "seller/member/login.jsp";
+    }
+
+    @RequestMapping("/seller/sign-up.do")
+    public String sellerRegister() {
+        return VIEW_PATH + "seller/member/sign-up.jsp";
+    }
+
+    @RequestMapping("/seller/cancel/list.do")
+    public String seller_customerCancelList() {
+        return VIEW_PATH + "seller/cancel/list.jsp";
+    }
+
+    @RequestMapping("/seller/cancel/list_detail.do")
+    public String seller_customerCancelListDetail() {
+        return VIEW_PATH + "seller/cancel/list_detail.jsp";
+    }
+
+    @RequestMapping("/seller/order/list.do")
+    public String sellerOrderList() {
+        return VIEW_PATH + "seller/order/list.jsp";
+    }
+
+    @RequestMapping("/seller/order/list_detail.do")
+    public String sellerOrderListDetail() {
+        return VIEW_PATH + "seller/order/list_detail.jsp";
+    }
+
+    @RequestMapping("/seller/product/edit.do")
+    public String sellerProductEdit() {
+        return VIEW_PATH + "seller/product/edit.jsp";
+    }
+
+    @RequestMapping("/seller/product/upload.do")
+    public String sellerProductUpload() {
+        return VIEW_PATH + "seller/product/upload.jsp";
+    }
+
+    @RequestMapping("/seller/product/view.do")
+    public String sellerProductView() {
+        return VIEW_PATH + "seller/product/view.jsp";
+    }
+    /* for seller end */
+
+    /* for customer start */
+    @RequestMapping("/login.do")
+    public String login() {
+        return VIEW_PATH + "/customer/member/login.jsp";
+    }
+
+    @RequestMapping("/sign-up.do")
+    public String sign_up() {
+        return VIEW_PATH + "/customer/member/sign-up.jsp";
+    }
+    /* for customer end */
 }
